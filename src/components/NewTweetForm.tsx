9c366b13c570a9ab6e8b05af176e81f9a2b354d4
Jason Gionfriddo <Ragtop66goat@gmail.com>
2023-12-21 22:53:21 -0600

    added route for profile and profile/id added sidenav and NewTweetForm.tsx

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,8 @@
+export default function NewTweetForm(){
+  return(
+    <>
+      // todo: 33:01 of the video
+      <h1>Form</h1>
+    </>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,31 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
+import {type Session} from "next-auth";
+import {SessionProvider} from "next-auth/react";
+import {type AppType} from "next/app";
 
-import { api } from "~/utils/api";
+import {api} from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
+import SideNav from "~/components/SideNav";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType<{ session: Session | null }> = ({Component, pageProps: {session, ...pageProps},}) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Thread Board</title>
+        <meta name="description" content="A place for professionals to communicate"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav/>
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error
 export default api.withTRPC(MyApp);
