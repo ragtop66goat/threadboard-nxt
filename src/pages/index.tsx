34e06b4016f51f24750e102941dc6148a49e440f
Jason Gionfriddo <Ragtop66goat@gmail.com>
2024-01-13 20:29:26 -0600

    added refeshing page when interacting with existing tweet functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { api } from "~/utils/api";
 export default function Home() {
   return (
     <>
-      todo: 1:23:00 of the tutorial
+      todo: 1:33:00 of the tutorial
       <header className="sticky top-0 z-10 border-b bg-white pt-2">
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
       </header>
